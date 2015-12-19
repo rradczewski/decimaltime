a4cbbdadaed7a9c1b2d34d7b5ba37b7f1e0114f8
Raimo Radczewski <raimo.radczewski@vaamo.de>
2015-12-19 15:23:00 +0100

    Basic functionality

---
+++ src/decimaltime.js
@@ -0,0 +1,43 @@
+
+function DecimalTime(hours, minutes, seconds, milliseconds, timezoneOffset) {
+    if(hours.constructor === Date) {
+        this.fraction = Date.utcFractionFromDate(hours);
+    } else {
+        this.fraction = Date.fromTime(hours | 0, minutes | 0, seconds | 0, milliseconds | 0, timezoneOffset | 0);
+    }
+}
+
+
+DecimalTime.prototype.getHours = function () {
+    return (this.fraction * 10) | 0;
+}
+
+DecimalTime.prototype.getMinutes = function () {
+    return ((this.fraction * 10 * 100) | 0) % 100;
+}
+
+DecimalTime.prototype.getSeconds = function () {
+    return ((this.fraction * 10 * 100 * 100) | 0) % 100;
+}
+
+DecimalTime.prototype.getMilliseconds = function () {
+    return ((this.fraction * 10 * 100 * 100 * 1000) | 0) % 1000;
+}
+
+Date.fromTime = function (hours, minutes, seconds, milliseconds, timezoneOffset) {
+    return (hours*60*60*1000 +
+        minutes*60*1000 +
+        seconds*1000 +
+        milliseconds +
+        timezoneOffset * 60 * 1000) /
+        (24*60*60*1000);
+}
+
+Date.utcFractionFromDate = function (date) {
+    return (date.getUTCHours()*60*60*1000 +
+           date.getUTCMinutes()*60*1000 +
+           date.getUTCSeconds()*1000 +
+           date.getUTCMilliseconds()) / (24*60*60*1000);
+}
+
+module.exports = DecimalTime;
---
+++ test/decimaltime.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var expect = require('chai').expect;
+var DecimalTime = require('../src/decimaltime');
+
+describe('the decimaltime base class', function () {
+
+    it('returns the decimal time for a provided date', function () {
+        var date = new Date('Sat Dec 19 2015 12:00:00.000 GMT+0000 (UTC)');
+        var d = new DecimalTime(date);
+        expect(d.fraction).to.eql(0.5);
+    });
+
+    it('returns the decimal time provided utc hours, minutes, seconds and milliseconds', function () {
+        var d = new DecimalTime(12, 34, 56, 789);
+
+        expect(d.getHours()).to.eql(5);
+        expect(d.getMinutes()).to.eql(24);
+        expect(d.getSeconds()).to.eql(26);
+        expect(d.getMilliseconds()).to.eql(839);
+    });
+
+    it('returns the different parts of a date', function () {
+        var date = new Date('Sat Dec 19 2015 12:34:56.789 GMT+0000 (UTC)');
+        var d = new DecimalTime(date);
+
+        expect(d.getHours()).to.eql(5);
+        expect(d.getMinutes()).to.eql(24);
+        expect(d.getSeconds()).to.eql(26);
+        expect(d.getMilliseconds()).to.eql(839);
+    });
+});
